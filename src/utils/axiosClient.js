3193e4905dc8356e9acd5b8298c3091844e7b68c
RohanPal1 <104245611+RohanPal1@users.noreply.github.com>
2024-01-22 15:20:54 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -9,8 +9,8 @@ import { TOAST_FAILURE } from '../App';
 //     withCredentials: true
 // })
 
-// let baseURL = 'http://localhost:4000';
-let baseURL = 'https://social-media-server-ashen.vercel.app/';
+ let baseURL = 'http://localhost:4000';
+//let baseURL = 'https://social-media-server-ashen.vercel.app/';
 console.log('env is ', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     baseURL = process.env.REACT_APP_SERVER_BASE_URL
